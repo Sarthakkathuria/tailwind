c9d2fd64a96c3410b2c924f7ce25fcf1230ac7cc
SarthakKathuria <sarthakkathuria2001@gmail.com>
2021-08-25 15:55:10 +0530

    Large Component Complete

--- components/LargeCard.js
+++ components/LargeCard.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import image from 'next/image';
 
-function LargeCard({img , }) {
+function LargeCard({img , title, description ,buttonText}) {
     return (
-        <div>
-            
+        <div className="relative py-16 cursor-pointer">
+            <div className="relative h-96 min-w-[300px]">
+                {/* <h1>fsdf</h1> */}
+                <img src= {img} className="2xl"/>
+                {/* <image src={img} layout="fill" objectFit="cover" /> */}
+                
+            </div>
+            <div className="absolute top-32 left-12">
+                <h3 className="text-4xl mb-3 w-64">{title}</h3>
+                <p>{description}</p>
+                <button className="text-sm text-white bg-gray-900 px-4 py-2 rounded-lg">{buttonText}</button>
+            </div>   
         </div>
     )
 }
