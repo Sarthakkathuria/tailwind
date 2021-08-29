4b1ac1e589255094197d8b7a5ebf08d6bc1eac36
SarthakKathuria <sarthakkathuria2001@gmail.com>
2021-08-29 12:34:38 +0530

    search page

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,31 @@ import {SearchIcon,
     UserCircleIcon,
     UserIcon,
 } from '@heroicons/react/solid';
-
-
+import { useState } from "react";
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 function Header() {
+
+    const [searchInput,setSearchInput] = useState("");
+    const [startDate,setStartDate] = useState(new Date());
+    const [endDate,setendDate] = useState(new Date());
+    const [guest,setGuest] = useState(1);
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: 'selection',
+    };
+
+    const resetInput = ()=> {
+        setSearchInput("");
+    };
+
+    // function reset
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setendDate(ranges.selection.endDate);
+    }
     return (
         // z index is used to always keep header on top and header is sticky and will stick at top for grid view 
         // and grid view has 3 col shadow   
@@ -24,7 +46,10 @@ function Header() {
         {/* Middle */}
         <div className="flex items-center md:border-2 rounded-full
         py-2 ">
-            <input className="flex-grow pl-5 bg-transparent outline-none text-sm" type="text" placeholder="Start your search"/>
+            <input 
+            value = {searchInput}
+            onChange= {(e) => setSearchInput(e.target.value)}
+            className="flex-grow pl-5 bg-transparent outline-none text-sm" type="text" placeholder="Start your search"/>
             {/* initially it is hidden once we use md:inline-flex it will be visible is full screen */}
             <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 icon
             text-white rounded-full p-2 cursor-pointer md:mx-2"/>
@@ -38,7 +63,27 @@ function Header() {
                 <UserCircleIcon className="h-6" />
             </div>
         </div>
+       {/* flex grow allows you yo extend to all the space available if we dont want to allow to grow flex=grow-0  */}
+       {/* col-span will allow to span across 3 colums of grid and mx-auto will adjust from both sides of div*/}
+        {searchInput && 
+        <div className ="flex flex-col col-span-3 mx-auto">
+            <DateRangePicker ranges={[selectionRange]}
+            minDate= {new Date()}
+            rangeColors= {["#FD5B61"]}
+            onChange={handleSelect}
+            />
+            <div className="flex items-center border-b mb-4">
+                <h2 className="text-2xl flex-grow font-semibold">Number of Guests</h2>
+                <UserIcon className='h-5'/>
+                <input value= {guest} onChange= {e => setGuest(e.target.value)} type="number" min={1} className="w-12 pl-2 text-lg outline-none text-red-400"/> 
+            </div>
+            <div className="flex">
+                <button onClick={resetInput} className="flex-grow text-gray-500">Cancel</button>
+                <button className="flex-grow text-red-500">Search</button>
+            </div>
 
+        </div>
+        }
         
         </header>   
     )
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
-
+import '../styles/global.css'
+//we have imported this to remove redundent code 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
